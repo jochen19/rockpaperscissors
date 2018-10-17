e4efb99d154138d32d9436d228b559fab11d52d5
Jonathan Chen <jonathanbaby88@gmail.com>
2018-10-17 01:02:49 +0000

    added random number generator

--- js/script.js
+++ js/script.js
@@ -9,9 +9,32 @@
 
 // DOCUMENT READY FUNCTION BELOW
 $(document).ready(function(){
+    var userChoice = "";
+    var computerChoice = "";
+    var winner = "";
+    var randomNumber = 0;
     $("#shoot").click(function(){
-       $("#userChoice").text($("#input").val()); 
+        randomNumber = Math.random();
+        userChoice = $("#input").val();
+        
+        $("#userChoice").text(userChoice); 
+        
+        if (randomNumber > .66){
+            computerChoice = "rock";
+        } else if (randomNumber < .66 && randomNumber > .33){
+            computerChoice = "paper";
+        } else {
+            computerChoice = "scissors";
+        }
+        
+        $("#computerChoice").text(computerChoice);
+        
+        if (userChoice === "rock" && computerChoice === "rock"){
+            $("#winner").text("Draw");
+        } else if (userChoice === "paper" && computerChoice === "rock"){
+            $("#winner").text("User");
+        } else if (userChoice === "scissors" && computerChoice === "rock"){
+            $("#winner").text("Computer");
+        }
     });
-    
-    
 });
